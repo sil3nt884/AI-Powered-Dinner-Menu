d13316d8ff901d2537786c07e22b08ed0cafc6f4
sil3n <silentsoul884@outlook.com>
2024-06-23 22:43:20 +0100

    local host all repice

--- web/dinner/next.config.mjs
+++ web/dinner/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         ignoreBuildErrors: true,
     },
     env: {
-        ADD_RECIPE_ENDPOINT: 'http://209.38.168.236:3000/addRecipes',
+        ADD_RECIPE_ENDPOINT: 'http://localhost:3000/addRecipes',
         ADD_INGREDIENT_ENDPOINT: 'http://209.38.168.236:3000/addIngredient',
         DINNERS_ENDPOINT:  'http://209.38.168.236:3000/dinners',
         GET_RECIPES_ENDPOINT: 'http://209.38.168.236:3000/getRecipes',
