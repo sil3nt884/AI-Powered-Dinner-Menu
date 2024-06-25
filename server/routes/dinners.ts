e95dbdc5210ad29fd4bcd79fd003201f05b32f4e
sil3n <silentsoul884@outlook.com>
2024-06-25 13:01:04 +0100

    create view  instead of query

--- server/routes/dinners.ts
+++ server/routes/dinners.ts
@@ -3,12 +3,7 @@ import { client } from '../pg';
 export const dinners = async (req: Request, resp: Response )=> {
     try {
         const dinnersQuery = await client.query(`
-            SELECT DISTINCT ON (DATE(wr.date), r.id) wr.recipe_id, r.name, r.url
-            FROM weekly_dinner wr
-                     JOIN recipes r ON r.id = wr.recipe_id
-            WHERE DATE(wr.date) >= (date_trunc('week', current_date) - INTERVAL '1 day')
-              AND DATE(wr.date) < (date_trunc('week', current_date) + INTERVAL '7 days');
-
+            SELECT * FROM weekly_dinner_view
         `)
         const dinners = dinnersQuery.rows;
         resp.status(200).send(dinners);
