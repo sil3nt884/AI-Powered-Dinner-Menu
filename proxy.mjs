0ad9d090ad1a68930ec9dce8de6eee95a1e56377
sil3n <silentsoul884@outlook.com>
2024-06-25 08:06:39 +0100

    https

--- proxy.mjs
+++ proxy.mjs
@@ -3,6 +3,14 @@ import redbird from 'redbird';
 // Create a Redbird instance with SSL support
 const proxy = redbird({
     port: 80, // Listen on port 80 for HTTP traffic
+    letsencrypt: {
+        path: __dirname + '/certs',
+        port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+    },
+    ssl: {
+        http2: true,
+        port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+    }
 });
 
 
