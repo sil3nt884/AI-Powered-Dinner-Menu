c54ae8cb1e580abde49a1fa38aef6450a1ddad19
sil3n <silentsoul884@outlook.com>
2024-06-25 08:04:40 +0100

    https

--- proxy.mjs
+++ proxy.mjs
@@ -3,16 +3,16 @@ import redbird from 'redbird';
 // Create a Redbird instance with SSL support
 const proxy = redbird({
     port: 80, // Listen on port 80 for HTTP traffic
-    ssl: {
-        port: 443, // Listen on port 443 for HTTPS traffic
-        key: '/path/to/your/ssl-key.pem', // Path to your SSL key
-        cert: '/path/to/your/ssl-cert.pem' // Path to your SSL certificate
-    }
 });
+
 
 // Register routes to forward traffic to your desired ports
 proxy.register('homeluu.ddns.net', 'http://localhost:3000', {
-    ssl: true
+    ssl: true,
+    letsencrypt: {
+        email: 'sil3nt994@gmail.com', // Domain owner/admin email
+    }
+
 });
 
 proxy.register('homeluu.ddns.net', 'http://localhost:3001', {
