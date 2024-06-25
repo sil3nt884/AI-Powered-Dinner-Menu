2ef076f3bc220dff0919e4aa624f80764260559c
sil3n <silentsoul884@outlook.com>
2024-06-25 09:33:52 +0100

    fix endpoints

--- web/dinner/next.config.mjs
+++ web/dinner/next.config.mjs
@@ -8,10 +8,10 @@ const nextConfig = {
     },
     env: {
         ADD_RECIPE_ENDPOINT: 'http://localhost:3000/addRecipes',
-        ADD_INGREDIENT_ENDPOINT: 'https://homeluu.ddns.net:3000/addIngredient',
-        DINNERS_ENDPOINT:  'https://homeluu.ddns.net:3000/dinners',
-        GET_RECIPES_ENDPOINT: 'https://homeluu.ddns.net:3000/getRecipes',
-        ADD_DINNER_ENDPOINT: 'https://homeluu.ddns.net:3000/addDinner',
+        ADD_INGREDIENT_ENDPOINT: 'https://homeluu.ddns.net/addIngredient',
+        DINNERS_ENDPOINT:  'https://homeluu.ddns.net/dinners',
+        GET_RECIPES_ENDPOINT: 'https://homeluu.ddns.net/getRecipes',
+        ADD_DINNER_ENDPOINT: 'https://homeluu.ddns.net/addDinner',
     }
 };
 
