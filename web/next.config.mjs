5287ce2cb899e4f266ec698a5fe1f0988cbcb007
sil3nt884 <ricky.luu911@gmail.com>
2024-07-08 18:18:58 +0100

    balh

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -21,8 +21,8 @@ const nextConfig = withPWA({
     env: {
         ADD_RECIPE_ENDPOINT: 'https://homeluu.ddns.net/addRecipes',
         ADD_INGREDIENT_ENDPOINT: 'https://homeluu.ddns.net/addIngredient',
-        DINNERS_ENDPOINT:  'https://localhost:4000/dinners',
-        GET_RECIPES_ENDPOINT: 'https://localhost:4000/getRecipes',
+        DINNERS_ENDPOINT:  'http://localhost:4000/dinners',
+        GET_RECIPES_ENDPOINT: 'http://localhost:4000/getRecipes',
         ADD_DINNER_ENDPOINT: 'https://homeluu.ddns.net/addDinner',
     }
 });
