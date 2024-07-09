a1f8ba7bb792bae4f470696dda491e10c9bae8c1
sil3nt884 <ricky.luu911@gmail.com>
2024-07-09 10:40:37 +0100

    send push

--- server/routes/workerTask.ts
+++ server/routes/workerTask.ts
@@ -1,6 +1,32 @@
 import { redisClient } from "../redis";
 import { commandOptions } from "redis";
 import { redisTasks } from "./addRecipe";
+import { client } from '../pg';
+import webPush from "web-push";
+
+
+const sendPushNotifications = async () => {
+        try {
+                const subscriptionsResults = await client.query('SELECT * FROM pushsubscription');
+                const subscriptions = subscriptionsResults.rows;
+
+
+                const notificationPayload = {
+                        title: "New Recipe Added",
+                        body: "New recipe added to the database"
+                }
+
+                await Promise.all(
+                    subscriptions.map(sub => webPush.sendNotification(sub, JSON.stringify(notificationPayload)))
+                )
+
+        }
+        catch (e) {
+                console.error(e);
+        }
+
+}
+
 
 export const handleTask = async () => {
         const redis = await redisClient().getClient();
@@ -14,6 +40,7 @@ export const handleTask = async () => {
         }
         const args = task.args;
         await redisTasks[taskName](JSON.parse(args));
+        await sendPushNotifications();
 
 }
 
